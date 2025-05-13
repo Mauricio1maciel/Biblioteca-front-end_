bceb18b6cb0f5721a87854c7d78ca5fcdb333fbf
Mauricio1maciel <macielmauriciio@gmail.com>
2025-05-13 20:05:19 -0300

    front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>
+      Bibliteca2025
+      </h1>
+      <button className='btn btn-primary'>Teste</button>
+      </>
+  );
+}
+
+export default App;
