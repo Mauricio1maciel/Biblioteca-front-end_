e871b15cf3d8d1ca4b8912b5e5d2444fa683db36
Mauricio1maciel <macielmauriciio@gmail.com>
2025-05-13 21:57:07 -0300

    Lista categoria e etc..

---
+++ front-end/src/componentes/TituloLista.js
@@ -0,0 +1,22 @@
+export default function Titulolista(props) {
+    return(
+        <>
+        <div className="container">
+  
+  <div className="row justify-content-md-center">
+    <div className="col">
+      <div className="alert alert-primary" role="alert">
+        <h4 className="alert-heading">{props.titulo}</h4>
+        <p>{props.descricao}</p>
+        <hr />
+        <a href={props.rota} className="btn btn-primary btn-md">
+          Novo
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+        </>
+    );
+};
